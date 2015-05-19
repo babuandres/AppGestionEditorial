ad045d172421bcbd877209187906dbd7c23e407a
babuandres <babuandres@hotmail.com>
2015-05-19 13:39:52 -0500

    Encapsulamiento

--- AppGestionEditorial/Modelo/Documentos.cs
+++ AppGestionEditorial/Modelo/Documentos.cs
@@ -8,15 +8,43 @@ namespace AppGestionEditorial.Modelo
     public class Documentos
     {
         #region "Atributos"
-        private string nombre;
-        private string codigo;
-        private string autor;
-        private string editorial;
-        private string FechaDePublicacion;
+        private string nombre;        
+        private string codigo;        
+        private string autor;        
+        private string editorial;        
+        private string fechaDePublicacion;        
         #endregion
 
         #region "Propiedades"
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
 
+        public string Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public string Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+
+        public string Editorial
+        {
+            get { return editorial; }
+            set { editorial = value; }
+        }
+
+        public string FechaDePublicacion
+        {
+            get { return fechaDePublicacion; }
+            set { fechaDePublicacion = value; }
+        }
         #endregion
     }
 }
