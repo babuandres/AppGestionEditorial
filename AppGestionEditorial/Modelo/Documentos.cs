2817a4c195a7b80a07d11ec5840516955b9c437a
babuandres <babuandres@hotmail.com>
2015-05-19 13:30:12 -0500

    Subida

---
+++ AppGestionEditorial/Modelo/Documentos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppGestionEditorial.Modelo
+{
+    public class Documentos
+    {
+        #region "Atributos"
+        private string nombre;
+        private string codigo;
+        private string autor;
+        private string editorial;
+        private string FechaDePublicacion;
+        #endregion
+
+        #region "Propiedades"
+
+        #endregion
+    }
+}
